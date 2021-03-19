835ba58bfe61065af965f6879783df03445cc8ad
iuriatan <iuriatan@gmail.com>
2021-03-19 16:34:40 -0300

    fix: deprecating nvm/yarn in favor of asdf

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -22,18 +22,6 @@
 		export PATH=$PATH:$HOME/fvm/default/bin
 	fi
 
-	# yarn global utilities
-	if [ -d $HOME/.yarn/bin ]; then
-		export PATH=$PATH:$HOME/.yarn/bin
-	fi
-
-	# nvm - node version manager
-	if [ -d $HOME/.nvm ]; then
-		export NVM_DIR="$HOME/.nvm"
-		[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-		[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-	fi
-
 	# go lang workspace
 	if [ -d $HOME/prog ]; then
 		export GOPATH=$HOME/prog/go
