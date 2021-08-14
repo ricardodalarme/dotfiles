47adb472f05005d00092a7e0df356ca7d8e64b1a
Iuriatan Felipe Muniz <iuriatan@profusion.mobi>
2021-08-13 23:38:51 -0300

    remove domain from hostname

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -6,8 +6,7 @@ setopt PROMPT_SUBST
 
 set_prompt() {
 
-	# [
-	PS1="%{$fg[white]%}[%{$fg_bold[white]%}%M %{$reset_color%}"
+	PS1="%{$fg[white]%}[%{$fg_bold[white]%}%m %{$reset_color%}"
 
 	# Path: http://stevelosh.com/blog/2010/02/my-extravagant-zsh-prompt/
 	PS1+="%{$fg_bold[cyan]%}${PWD/#$HOME/~}%{$reset_color%}"
