2f8cd43e1d1666f0ff35dee5dac34aed59267696
ricardodalarme <ricardo.dalarme@profusion.mobi>
2024-04-23 22:19:07 -0300

    refactor: remove unused scripts

--- function_exportenv.sh
+++
@@ -1,7 +0,0 @@
-function exportenv() {
-	# fails on SMTP_EMAIL_ALIAS="Delivery Center - Acompanhe seu pedido"
-	export $(cat $1 | sed 's/#.*//g' | xargs -0) 
-
-	# fails on OPERATION_JOBS_INTEGRATE="*/1 * * * *"
-	# set -o allexport; source $1; set +o allexport 
-}
--- function_randpass.sh
+++
@@ -1,3 +0,0 @@
-function randpass(){
-	openssl rand -base64 ${1:-16} | cut -b -${1:-16}
-}
